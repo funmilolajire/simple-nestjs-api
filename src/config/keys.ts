f4c0d475e20264e205c3eec50b94e19f1c2b8222
Funmilola Olajire <funmilolajire@gmail.com>
2021-04-07 16:53:14 +0100

    Delete src/config directory

--- src/config/keys.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    mongoURI: 'mongodb+srv://erinayo:Lembers6A.@erinscluster.pdqer.mongodb.net/items?retryWrites=true&w=majority',
-    options: { useNewUrlParser: true, useUnifiedTopology: true }
-}
