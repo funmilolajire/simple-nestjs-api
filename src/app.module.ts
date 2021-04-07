465ba7bd287a3b8273266694f773a2a54411742f
Funmilola O <funmilolajire@gmail.com>
2021-04-07 17:23:24 +0100

    changes to config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import config from './configs/keys'
+import config from './config/keys'
 
 @Module({
   imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI, config.options),],
